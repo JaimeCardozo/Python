69fadad1fdacf5afb3dfbf3bdc1180aec992603f
JaimeCardozo <jecardozo@uninorte.edu.co>
2022-12-20 19:29:55 -0500

    conditionals

---
+++ conditionals/compare.py
@@ -0,0 +1,9 @@
+#It's simple way compare two numbers 
+number1 = int(input("what's number1? "))
+number2 = int(input("what's number2? "))
+if number1 < number2:
+   print("the number 1 is less than number 2")
+if number1>number2:
+   print("the number 1 is greater than number 2")
+if number1 == number2:
+   print("the number 1 is equal to number 2")

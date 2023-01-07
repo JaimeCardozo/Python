6c2febcf0aa92c19ac4a2e2bbc518d74fc1cf6fe
JaimeCardozo <jecardozo@uninorte.edu.co>
2023-01-07 14:24:07 -0500

    meal

--- Set_1/meal.py
+++ Set_1/meal.py
@@ -1,10 +1,19 @@
-def main: 
+def main(): 
     msg = input("what time is it? ")
     msg = msg.strip()
     msg = msg.replace(" ","")
-    
-    print(msg)
+    time = convert(msg)
+    if time>=7 and time <= 8:
+        print("breakfast time")
+    elif time>=12 and time <= 13:
+        print("lunch time")
+    elif time >=18 and time<= 19:
+        print("dinner time")
+
 def convert(time):
-    
+    position = time.find(":")
+    hour = float(time[0:position])
+    minutes = float(time[position+1:len(time)])
+    return float(hour + minutes/60)
 
 main()    

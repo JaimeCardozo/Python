7d42dfd7fcf2473d4f9e791317157e934a010371
JaimeCardozo <jecardozo@uninorte.edu.co>
2023-01-13 16:42:31 -0500

    list, dic

---
+++ loops/cat.py
@@ -0,0 +1,12 @@
+def main():
+    number = get_number()
+    meow(number)
+def get_number():
+    while True:
+        n = int(input("What's is n? "))
+        if n>0:
+            return n
+def meow(n):
+    for i in range(n):
+        print("meow")
+main()            
---
+++ loops/students.py
@@ -0,0 +1,6 @@
+#this code is about simple list 
+def main ():
+    students = ["Hermione", "Harry", "Ron"]
+    for i in students:
+            print(i)
+main()
